3f2d200757e00d1ecad8abd524ca31747af94d96
aseryykazak <a.seryykazak@mail.ru>
2023-06-06 21:24:18 +0300

    test_06.06.23_1.2

--- main.py
+++ main.py
@@ -21,12 +21,46 @@
 # answer = int(input("Enter some digits: "))
 # input(answer)
 
-number = int(input("Enter some digits: "))
+# number = int(input("Enter some digits: "))
+# number_2 = int(input("Enter some digits: "))
+#
+# answer_1 = number + number_2
+# answer_2 = number - number_2
+# answer_3 = number * number_2
+#
+# print(answer_1, answer_2, answer_3)
+
+# True
+# False
+
+# print(type(True))
+# print(5!=2)
+
+# value = int(input("Enter first digits: "))
+# value_2 = int(input("Enter second digits: "))
+# if value_2 ==0:
+#     print("Zero division problem")
+# else:
+#     answer = value / value_2
+#    print(answer)
+
+
+# fingers = int(input("Input number of fingers "))
+# if fingers == 1:
+#     print("Штрафной бросок")
+# elif fingers == 3:
+#     print("Трехочковый бросок")
+# elif fingers == 2:
+#     print("Обычный - 2 очка")
+# else:
+#     print("Что происходит???")
+
+number_1 = int(input("Enter some digits: "))
 number_2 = int(input("Enter some digits: "))
-
-answer_1 = number + number_2
-answer_2 = number - number_2
-answer_3 = number * number_2
-
-print(answer_1, answer_2, answer_3)
+if number_1 > number_2:
+    print(number_2)
+elif number_1 < number_2:
+    print(number_1)
+else:
+    print("Они равны")
 
