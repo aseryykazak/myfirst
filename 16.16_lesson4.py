a00e56c6cdee6b385cd69954f0e0c85032cd4907
aseryykazak <a.seryykazak@mail.ru>
2023-06-16 19:39:30 +0300

    16.06.23_1

---
+++ 16.16_lesson4.py
@@ -0,0 +1,10 @@
+# d1 = {'w': 50, 'x': 100, 'y': 55, 'z': 400}
+# lst = [1,2,3,4,5]
+# for k, v in d1.items():
+#     print(k, ">", v)
+
+
+d1 = {'w': 50, 'x': 100, 'y': 55, 'z': 400}
+lst = [1,2,3,4,5]
+total = sum(d1.values())
+print(total)
