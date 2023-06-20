29c89359691c03704f04c971d9d0ca2da9ba740f
aseryykazak <a.seryykazak@mail.ru>
2023-06-20 21:50:36 +0300

    20.06.23_3

---
+++ functions/function_a_b.py
@@ -0,0 +1,12 @@
+x = input()
+y = input()
+z = input()
+def a(x, y, z):
+    return x, y, z
+    print(a)
+
+def b(rng):
+    rng = range(x, y, z)
+    for n in rng:
+        return rng
+        print(n)
