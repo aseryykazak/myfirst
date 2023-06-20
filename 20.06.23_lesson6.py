2e14706aa3397d2da0aecba356310ab7aca69f37
aseryykazak <a.seryykazak@mail.ru>
2023-06-20 19:44:03 +0300

    20.06.23_2

--- 20.06.23_lesson6.py
+++ 20.06.23_lesson6.py
@@ -1,8 +1,18 @@
 
-def tem(x, y, gr):
-    if gr == "F":
-        return x * 1.8 + 32
-    elif gr == "C":
-        return (y - 32)/1.8
-print(tem(5, 10, "F"))
+# def tem(x, y, gr):
+#     if gr == "F":
+#         return x * 1.8 + 32
+#     elif gr == "C":
+#         return (y - 32)/1.8
+# print(tem(5, 10, "C"))
+
+def number(x):
+    if x % 4 ==0 and x % 100!=0 or x % 400==0 or x % 400==0:
+        return "YES"
+    else:
+        return "NO"
+print(number(2023))
+
+
+
 
